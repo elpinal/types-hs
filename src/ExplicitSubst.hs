3316439ed36e470e6dc14f4f15da3120bce3266b
elpinal <elpinal@users.noreply.github.com>
2018-09-24 22:08:26 +0900

    Add reduce functions

--- src/ExplicitSubst.hs
+++ src/ExplicitSubst.hs
@@ -9,7 +9,11 @@ module ExplicitSubst
   , Subst(..)
   , ETerm(..)
   , foldExpr
+  , nf
+  , reduce
   ) where
+
+import Control.Monad.Trans.State
 
 data Expr f = In (f (Expr f))
 
@@ -89,3 +93,54 @@ instance Render Explicit where
 instance Render ETerm where
   render (Inl t) = render t
   render (Inr e) = render e
+
+class Whnf f where
+  whnf :: f (Expr ETerm) -> State (Subst (Expr ETerm)) (Expr ETerm)
+
+instance Whnf Term where
+  whnf (Var n) = do
+    s <- get
+    case s of
+      Id -> return $ var n
+      Shift -> put Id >> return (var $ n + 1)
+      Cons t s' -> case n of
+        0 -> case t of
+          (In (Inr (Explicit (In t) s0))) -> put s0 >> whnf t
+          _ -> error $ "unexpected error: " ++ pretty t
+        _ -> put s' >> whnf (Var $ n - 1)
+      Comp s1 s2 -> put s2 >> whnf (Inr $ Explicit (var n) s1)
+  whnf t @ (Abs _) = return $ In $ Inl t
+  whnf (App (In t1) t2) = do
+    s <- get
+    t1' <- whnf t1
+    case t1' of
+      In (Inl (Abs (In t))) -> withState (Cons $ explicit t2 s) $ whnf t
+      _ -> return $ app t1' $ explicit t2 s
+
+instance Whnf Explicit where
+  whnf (Explicit (In (Inl (Var n))) s0) = case s0 of
+    Id -> whnf $ Var n
+    Shift -> whnf $ Var $ n + 1
+    Cons (In t) s -> case n of
+      0 -> whnf t
+      _ -> whnf $ Explicit (var $ n - 1) s
+    Comp s1 s2 -> withState (Comp s2) $ whnf $ Inr $ Explicit (var n) s1
+  whnf (Explicit (In t) s) = withState (Comp s) $ whnf t
+
+instance Whnf ETerm where
+  whnf (Inl t) = whnf t
+  whnf (Inr e) = whnf e
+
+nf :: Whnf f => f (Expr ETerm) -> Subst (Expr ETerm) -> Expr ETerm
+nf x s0 =
+  let (In (Inl y), s) = runState (whnf x) s0 in
+    case y of
+      Abs (In a) -> lam $ nf a $ (explicit (var 0) Id) `Cons` (Comp s Shift)
+      App a b -> f a b
+        where
+          f (In (Inl (App x y))) (In (Inr (Explicit (In a) s))) = f x y `app` nf a s
+          f a (In (Inr (Explicit (In b) s))) = app a $ nf b s
+      _ -> term y
+
+reduce :: Expr ETerm -> Expr ETerm
+reduce (In e) = nf e Id
